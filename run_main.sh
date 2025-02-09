ad5d120150bafca33a39d236e5528e9f0387dd4f
fucai <fke@nec-labs.com>
2025-02-08 22:04:30 -0800

    update image_patch llm_query

--- run_main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# preprocessing
-python main.py \
-    --data_root '/home/mai/fke/fkee/coco2014' \
-    --base_config './config/okvqa.yaml' \
-    --model_config './config/model_config_1gpu.yaml' 
-echo "preprocessing done"

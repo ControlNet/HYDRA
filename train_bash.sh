b4a07e1a6857793fbe3e148898a0654b3609eedd
fucai <fke@nec-labs.com>
2025-02-10 03:10:18 -0800

    update readme

--- train_bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# preprocessing
-python train.py \
-    --data_root '/home/mai/fke/fkee/coco2014' \
-    --base_config './config/okvqa.yaml' \
-    --model_config './config/model_config_1gpu.yaml' \
-    --dqn_config './config/dqn_debug.yaml' 
-echo "preprocessing done"
